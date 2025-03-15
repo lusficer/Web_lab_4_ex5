39e828b2196997ecabfb7132ab931177d51856e6
Bạch Huy Hoàng <108335315+lusficer@users.noreply.github.com>
2025-03-15 13:59:23 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/web_lab_4_ex1/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.web_lab_4_ex1;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
